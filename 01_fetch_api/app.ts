e7c2cdaa789ab5fe8f11dea8523ae62208570a7c
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-12-27 02:34:02 +0630

    added printTodo function with explict types

--- 01_fetch_api/app.ts
+++ 01_fetch_api/app.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 interface Todo {
-    userId: number;
     id: number;
     title: string;
     completed: boolean;
@@ -11,10 +10,17 @@ interface Todo {
 
 axios.get(url).then(res => {
     const data = res.data as Todo;
-    const userId = data.userId;
     const id = data.id;
     const title = data.title;
     const completed = data.completed;
 
-    console.log(userId, id, title, completed)
+    printTodo(id, title, completed);
 })
+
+function printTodo(id: number, title: string, completed: boolean) {
+    console.log(`
+    ID ${id}
+    Has a title ${title}
+    Is it completed? ${completed}
+    `);
+}
