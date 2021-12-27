e351b873520b430a1191a033baee0c6c8cbbc06e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-12-28 00:32:10 +0630

    basic type annotations

---
+++ 02_types/annotations/variables.ts
@@ -0,0 +1,8 @@
+let apples: number = 5
+let hasGirlFriend: boolean = false
+let myName: string = "Aung Myat Moe"
+let nothing: null = null
+let notDefined: undefined = undefined
+
+// built-in class
+let now: Date = new Date()
