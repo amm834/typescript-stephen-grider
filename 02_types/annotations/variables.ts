b3a0638bd9e0c473b529e9c543c2923b8042f68a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-12-28 00:41:55 +0630

    More complex types

--- 02_types/annotations/variables.ts
+++ 02_types/annotations/variables.ts
@@ -1,8 +1,28 @@
+// primitive
 let apples: number = 5
 let hasGirlFriend: boolean = false
 let myName: string = "Aung Myat Moe"
 let nothing: null = null
 let notDefined: undefined = undefined
 
-// built-in class
+// built-in object
 let now: Date = new Date()
+
+// class
+class Person {
+}
+
+let person: Person = new Person()
+
+// Array
+let numbers: number[] = [1, 2, 3, 4, 5]
+let truths: boolean[] = [true, false, true]
+
+// Object literals
+let points: {
+    x: number;
+    y: number;
+} = {
+    x: 10,
+    y: 10
+}
