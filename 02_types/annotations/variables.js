a46eae115d4118f55aff37e4a1f2f09c9a1b19f0
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-12-29 02:27:27 +0630

    union type

--- 02_types/annotations/variables.js
+++ 02_types/annotations/variables.js
@@ -25,5 +25,22 @@ var logNumber = function (num) {
     console.log(num);
 };
 logNumber(2);
+// 1) When we are using any type and typescript cannot guess
 var json = "{\"x\":20,\"y\":30}";
 var coordinates = JSON.parse(json);
+// 2) When initilization is later
+var colors = ['red', 'green', 'blue'];
+var isGreen;
+for (var color in colors) {
+    if (color === 'green') {
+        isGreen = true;
+    }
+}
+// 3) When type inference work incorrectly
+var nums = [-10, -33, 33];
+var numAboveZero = false;
+for (var i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+        numAboveZero = nums[i];
+    }
+}
--- 02_types/annotations/variables.ts
+++ 02_types/annotations/variables.ts
@@ -33,5 +33,24 @@ const logNumber: (num: number) => void = (num: number) => {
 }
 logNumber(2)
 
+// 1) When we are using any type and typescript cannot guess
 let json = `{"x":20,"y":30}`
 let coordinates: { x: number; y: number } = JSON.parse(json)
+
+// 2) When initilization is later
+let colors: string[] = ['red', 'green', 'blue']
+let isGreen: boolean;
+for (const color in colors) {
+    if (color === 'green') {
+        isGreen = true
+    }
+}
+
+// 3) When type inference work incorrectly
+let nums = [-10, -33, 33]
+let numAboveZero: boolean | number = false
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+        numAboveZero = nums[i]
+    }
+}
