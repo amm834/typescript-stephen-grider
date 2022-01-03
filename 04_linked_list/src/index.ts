073e42a9e001b03f4a3d11c182a7bc0a86d50ae3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-03 21:52:58 +0630

    Implemented bubble sort

--- 04_linked_list/src/index.ts
+++ 04_linked_list/src/index.ts
@@ -1,8 +1,20 @@
 class Sorter {
-    constructor(public collection: number[]) {}
+  constructor(public collection: number[]) {}
 
-    sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
 }
 
 const sorter = new Sorter([20, 30, -1, 5]);
 sorter.sort();
+console.log(sorter.collection);
