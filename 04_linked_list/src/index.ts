267610caba45dd6d50de58477c7ca7fd624780a7
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-03 22:15:38 +0630

    Added todo

--- 04_linked_list/src/index.ts
+++ 04_linked_list/src/index.ts
@@ -1,18 +1,21 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+    constructor(public collection: number[] | string) {}
 
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+    sort(): void {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            // If collection is a number array
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+
+            // If collection is a string
         }
-      }
     }
-  }
 }
 
 const sorter = new Sorter([20, 30, -1, 5]);
