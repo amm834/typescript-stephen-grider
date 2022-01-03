5e34fd54a05f246bf6ed6910dbad1c3250e4ae3a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-03 22:32:36 +0630

    Added type guard (just checking the type)

--- 04_linked_list/src/index.ts
+++ 04_linked_list/src/index.ts
@@ -5,15 +5,19 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             // If collection is a number array
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+            if (this.collection instanceof Array) {
+                for (let j = 0; j < length - i - 1; j++) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
                 }
             }
 
             // If collection is a string
+            if (typeof this.collection === 'string') {
+            }
         }
     }
 }
