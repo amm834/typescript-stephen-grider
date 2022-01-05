e5253f4f032099ed253caaeb619d48557420763b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-05 20:46:53 +0630

    CharacterCollection class impleted

--- 04_linked_list/src/index.ts
+++ 04_linked_list/src/index.ts
@@ -1,7 +1,13 @@
 import { NumberCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 import { Sorter } from './Sorter';
 
-const numbesCollection = new NumberCollection([10, 2, -5, 6]);
-const sorter = new Sorter(numbesCollection);
+// const numbesCollection = new NumberCollection([10, 2, -5, 6]);
+// const sorter = new Sorter(numbesCollection);
+// sorter.sort();
+// console.log(numbesCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaay');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbesCollection.data);
+console.log(charactersCollection);
