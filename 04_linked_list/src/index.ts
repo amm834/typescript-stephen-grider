86fb6776bbc9d6949746afa94c980034c2b14f69
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-05 20:13:04 +0630

    Refratoring the Sorter class

--- 04_linked_list/src/index.ts
+++ 04_linked_list/src/index.ts
@@ -1,27 +1,7 @@
-class Sorter {
-    constructor(public collection: number[] | string) {}
+import { NumberCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
 
-    sort(): void {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            // If collection is a number array
-            if (this.collection instanceof Array) {
-                for (let j = 0; j < length - i - 1; j++) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-            }
-
-            // If collection is a string
-            if (typeof this.collection === 'string') {
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([20, 30, -1, 5]);
+const numbesCollection = new NumberCollection([10, 2, -5, 6]);
+const sorter = new Sorter(numbesCollection);
 sorter.sort();
 console.log(sorter.collection);
