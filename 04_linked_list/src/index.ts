2aaf150df51aba7f2b52a1d974cfc4da487b1ed9
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-05 20:37:37 +0630

    Sortable interface implemented

--- 04_linked_list/src/index.ts
+++ 04_linked_list/src/index.ts
@@ -4,4 +4,4 @@ import { Sorter } from './Sorter';
 const numbesCollection = new NumberCollection([10, 2, -5, 6]);
 const sorter = new Sorter(numbesCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbesCollection.data);
