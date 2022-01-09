c6cee00bd3bb720d2e79d8dfdb46329676393499
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-09 19:29:40 +0630

    Parse CSV

--- 05_stats/src/index.ts
+++ 05_stats/src/index.ts
@@ -1,3 +1,11 @@
 import fs from 'fs';
 
-console.log(fs);
+const matches = fs
+    .readFileSync('football.csv', {
+        encoding: 'utf-8',
+    })
+    .split('\n')
+    .map((row: string): string[] => {
+        return row.split(',');
+    });
+console.log(matches);
