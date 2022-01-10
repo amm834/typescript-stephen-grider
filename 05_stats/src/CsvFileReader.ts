2764f391d8b0421c30a6a4021f236c351abdf888
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-10 22:20:27 +0630

    Converting to Date and Number

--- 05_stats/src/CsvFileReader.ts
+++ 05_stats/src/CsvFileReader.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import {dateStringToDate} from "./utils";
 
 export class CsvFileReader {
     public data: string[][] = []
@@ -14,6 +15,17 @@ export class CsvFileReader {
             .split('\n')
             .map((row: string): string[] => {
                 return row.split(',');
-            });
+            })
+            .map((row: string[]): any => {
+                return [
+                    dateStringToDate(row[0]),
+                    row[1],
+                    row[2],
+                    Number(row[3]),
+                    Number(row[4]),
+                    row[5],
+                    row[6]
+                ]
+            })
     }
 }
