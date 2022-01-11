469219f65aa5b850997851efd1e9f5da0624410b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-12 01:08:43 +0630

    Putting it all together

--- 05_stats/build/index.js
+++ 05_stats/build/index.js
@@ -2,6 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
+const Summary_1 = require("./Summary");
+const WinsAnalyzer_1 = require("./analyzers/WinsAnalyzer");
+const ConsoleReport_1 = require("./outputTargets/ConsoleReport");
 const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
+const summary = new Summary_1.Summary(new WinsAnalyzer_1.WinsAnalyzer('Man United'), new ConsoleReport_1.ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
--- 05_stats/src/index.ts
+++ 05_stats/src/index.ts
@@ -1,7 +1,15 @@
 import {CsvFileReader} from "./CsvFileReader";
 import {MatchReader} from "./MatchReader";
+import {Summary} from "./Summary";
+import {WinsAnalyzer} from "./analyzers/WinsAnalyzer";
+import {ConsoleReport} from "./outputTargets/ConsoleReport";
 
 const csvFileReader = new CsvFileReader('football.csv')
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
+const summary = new Summary(
+    new WinsAnalyzer('Man United'),
+    new ConsoleReport()
+)
+summary.buildAndPrintReport(matchReader.matches)
