c024772e984ca3ba88a8f7054e8528a0be4e1ec8
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-12 22:19:30 +0630

    Generic Constraint

--- 02_types/generic.ts
+++ 02_types/generic.ts
@@ -36,10 +36,34 @@ function printString(arr: string[]): void {
     }
 }
 
-function printAnything(arr: T): void {
+function printAnything<T>(arr: T[]): void {
     for (let i = 0; i < arr.length; i++) {
         console.log(arr[i])
     }
 }
 
-printAnything(['a', 'b', 'c'])
+printAnything<string>(['a', 'b', 'c'])
+
+// Generic Constraint
+class Car {
+    print(): void {
+        console.log('I am a car')
+    }
+}
+
+class House {
+    print(): void {
+        console.log('I am a house')
+    }
+}
+
+interface Printable {
+    print(): void
+}
+function printCarOrHouse<T extends Printable>(arr: T[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].print()
+    }
+}
+
+printCarOrHouse<House>([new House(), new Car()])
