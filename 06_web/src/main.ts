eff9b2ec9931da559e4edee7d8384392ceb1e8c3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-13 02:21:11 +0630

    Optional interface property

--- 06_web/src/main.ts
+++ 06_web/src/main.ts
@@ -2,7 +2,9 @@ import './style.css'
 import {User} from "./models/User";
 
 const user = new User({
-    name:"John",
-    age:18
+    name: "John",
+    age: 18
 })
+
 console.log(user.get('name'))
+console.log(user.get('age'))
