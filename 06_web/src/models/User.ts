d1b2269a843675a08327e9b95f417d1b37f2e13b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-14 03:45:07 +0630

    Final user model refractoring

--- 06_web/src/models/User.ts
+++ 06_web/src/models/User.ts
@@ -1,4 +1,7 @@
 import {Model} from "./Model";
+import {Attributes} from "./Attributes";
+import {ApiSync} from "./ApiSync";
+import {Eventing} from "./Eventing";
 
 export interface UserProps {
     id?: number;
@@ -11,9 +14,12 @@ const rootUrl = 'http://localhost:3000/users'
 
 export class User extends Model<UserProps> {
 
-    constructor(data: UserProps) {
-        super(props);
-
+    static buildUser(data: UserProps): User {
+        return new User(
+            new Attributes<UserProps>(data),
+            new ApiSync<UserProps>(rootUrl),
+            new Eventing()
+        )
     }
 
 }
