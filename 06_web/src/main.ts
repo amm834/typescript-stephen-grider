224f6ddcb75fd063fb887c011b8299769498754a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-16 02:41:34 +0630

    Testing the UserEdit

--- 06_web/src/main.ts
+++ 06_web/src/main.ts
@@ -1,11 +1,12 @@
-import {UserForm} from "./views/UserForm";
 import {User} from "./models/User";
+import {UserEdit} from "./views/UserEdit";
 
 const user = User.build({name: "NAME", age: 20})
 const root = document.getElementById('app')
 if (root) {
-    const userForm = new UserForm(root, user)
+    const userForm = new UserEdit(root, user)
     userForm.render()
-}else {
+    console.log(userForm)
+} else {
     throw new Error('root Element not found')
 }
