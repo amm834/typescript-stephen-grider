1dffeeb392ab7670469dc932a3f9c0a57ec3600f
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-16 01:41:26 +0630

    Save model data to the db

--- 06_web/src/views/UserForm.ts
+++ 06_web/src/views/UserForm.ts
@@ -7,9 +7,13 @@ export class UserForm extends View<User, UserProps> {
     eventsMap(): { [key: string]: () => void } {
         return {
             'click:.set-age': this.onSetAgeClick,
-            'click:.set-name': this.onSetNameClick
+            'click:.set-name': this.onSetNameClick,
+            'click:.save-user': this.onSaveUserClick
         }
+    }
 
+    onSaveUserClick = (): void => {
+        this.model.save();
     }
 
     onSetNameClick = (): void => {
@@ -25,18 +29,16 @@ export class UserForm extends View<User, UserProps> {
     }
 
     template(): string {
-        return `
+        return (
+            `
             <div>
-                <h1>User Form </h1>
-                <div>
-                    <p>User name is ${this.model.get('name')}</p>
-                    <p>User age is ${this.model.get('age')}</p>
-                </div>
-                <input />
+                <input placeholder="Enter Username"/>
                 <button class="set-name">Change Name</button>
                 <button class="set-age">Set Random Age</button>
+                <button class="save-user">Save</button>
             </div>
         `
+        )
     }
 
 
