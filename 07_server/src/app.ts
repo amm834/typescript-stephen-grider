3ba99724ee07f4d844ca449ac996ee6bb6ff9f92
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-16 04:17:07 +0630

    Express basics

--- 07_server/src/app.ts
+++ 07_server/src/app.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express, {Request, Response} from "express"
+
+const app = express()
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+    <div>
+        <h1>Hi There</h1>
+    </div>
+    `)
+})
+app.listen(3000,() => {
+    console.log('Application is running on port 3000')
+})
