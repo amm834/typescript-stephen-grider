637f875f07875586d53013ea1c6dce587b656ba2
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-15 21:30:26 +0630

    Testing render()

--- 06_web/src/main.ts
+++ 06_web/src/main.ts
@@ -1,9 +1,4 @@
-import {User} from "./models/User";
+import {UserForm} from "./views/UserForm";
 
-const collection = User.buildCollection();
-
-collection.on('change', () => {
-    console.log(collection)
-})
-
-collection.fetch()
+const userForm = new UserForm(document.getElementById('app'))
+userForm.render()
