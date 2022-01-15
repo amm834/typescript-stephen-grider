09af57995c5008389db10e3c4e88bf80e086d5a2
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-15 20:55:50 +0630

    Changing collection class to reuseable class

--- 06_web/src/models/Collection.ts
+++ 06_web/src/models/Collection.ts
@@ -1,12 +1,14 @@
-import {User, UserProps} from "./User";
 import {Eventing} from "./Eventing";
 import axios, {AxiosResponse} from "axios";
 
-export class Collection {
-    models: User[] = [];
+export class Collection<T, K> {
+    models: T[] = [];
     events: Eventing = new Eventing();
 
-    constructor(public rootUrl: string) {
+    constructor(
+        public rootUrl: string,
+        public deserialize: (json: K) => T
+    ) {
     }
 
     get on() {
@@ -19,9 +21,8 @@ export class Collection {
 
     fetch(): void {
         axios.get(this.rootUrl).then((response: AxiosResponse) => {
-            response.data.forEach((value: UserProps): void => {
-                const user = User.build(value)
-                this.models.push(user)
+            response.data.forEach((value: K): void => {
+                this.models.push(this.deserialize(value))
             })
             this.trigger('change')
         })
