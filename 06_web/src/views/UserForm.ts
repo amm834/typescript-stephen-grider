b0ab5162ae42eafb40533a78e5750467b2a46c12
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-15 22:38:00 +0630

    Re-rendering dom

--- 06_web/src/views/UserForm.ts
+++ 06_web/src/views/UserForm.ts
@@ -3,6 +3,13 @@ import {User} from "../models/User";
 export class UserForm {
 
     constructor(public parent: Element, public model: User) {
+        this.bindModel()
+    }
+
+    bindModel(): void {
+        this.model.on('change', () => {
+            this.render()
+        })
     }
 
     eventsMap(): { [key: string]: () => void } {
@@ -13,7 +20,7 @@ export class UserForm {
     }
 
     onSetAgeClick = (): void => {
-       this.model.setRandomAge()
+        this.model.setRandomAge()
     }
 
     template(): string {
@@ -43,6 +50,9 @@ export class UserForm {
     }
 
     render(): void {
+        // Remove every html element before render
+        this.parent.innerHTML = '';
+
         const templateElement = document.createElement('template')
         templateElement.innerHTML = this.template();
         this.bindEvents(templateElement.content)
