4e70f8a91110101f003340bb727deb2fa240cdd9
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-17 00:47:54 +0630

    Decorator around properties

--- 02_features/decorator.ts
+++ 02_features/decorator.ts
@@ -1,4 +1,5 @@
 class Boat {
+    @testDecorator
     color: string = 'red'
 
     get formattedColor(): string {
@@ -10,6 +11,10 @@ class Boat {
         throw new Error()
     }
 
+}
+
+function testDecorator(target: any, key: string) {
+    console.log(target[key])
 }
 
 function logError(errorMessage: string) {
