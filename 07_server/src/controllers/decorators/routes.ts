b360940097f50d93b5ee82b9dcb27fb18b201acc
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-17 22:33:46 +0630

    Controller decorator

--- 07_server/src/controllers/decorators/routes.ts
+++ 07_server/src/controllers/decorators/routes.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 
 export function get(path: string) {
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    return function (target: Object, key: string, desc: PropertyDescriptor) {
         Reflect.defineMetadata('path', path, target, key);
     }
 }
