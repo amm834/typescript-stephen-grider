19806f808ca859a79e21fe727c8751266c0176d6
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-17 21:02:33 +0630

    Using meta data with decorator fuction

---
+++ 02_features/meta_in_practice.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata'
+
+class Plane {
+    color: string = 'red'
+
+    @markFunction
+    fly() {
+        console.log('vrrrrr')
+    }
+}
+
+function markFunction(target: Plane, key: string) {
+    Reflect.defineMetadata('secret', 123, target, key)
+}
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly')
+console.log(secret)
