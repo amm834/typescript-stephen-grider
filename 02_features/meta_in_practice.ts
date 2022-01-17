3bfa3fb3cd8dc03481ff2387b950a1e19773c499
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-17 21:06:00 +0630

    Using meta data with decorator factory

--- 02_features/meta_in_practice.ts
+++ 02_features/meta_in_practice.ts
@@ -3,14 +3,17 @@ import 'reflect-metadata'
 class Plane {
     color: string = 'red'
 
-    @markFunction
+    @markFunction('Hello World!')
     fly() {
         console.log('vrrrrr')
     }
 }
 
-function markFunction(target: Plane, key: string) {
-    Reflect.defineMetadata('secret', 123, target, key)
+function markFunction(info: string) {
+    return function (target: Plane, key: string) {
+        Reflect.defineMetadata('secret', 123, target, key)
+    }
+
 }
 
 const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly')
