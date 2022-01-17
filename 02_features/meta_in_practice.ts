f1ec7dd12a96b1374a20c8f7df5e339e431bbe28
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-18 02:49:45 +0630

    Implementing dummy controller that is not compatiable in tutorial

--- 02_features/meta_in_practice.ts
+++ 02_features/meta_in_practice.ts
@@ -1,23 +1,49 @@
 import 'reflect-metadata'
 
-@controller
+// Dummy router
+class Router {
+    static get(path: string, handler: () => void) {
+        handler()
+    }
+}
+
+@controller('/auth')
 class LoginController {
-    color: string = 'red'
 
     @get('/login')
-    login(): void {
-        console.log('login method')
+    login() {
+        console.log('I am logged in.')
+    }
+
+    @get('/logout')
+    logout() {
+        console.log('some')
     }
 }
 
 
-
 function get(path: string) {
-    return (target: LoginController, key: string): void => {
+    return function (target: any, key: string) {
         Reflect.defineMetadata('path', path, target, key)
     }
 }
 
-function controller(target:typeof LoginController) {
-    console.log(target.toString())
+// Implemented by Aung Myat Moe - @amm834 - MIT license
+function controller(prefix: string) {
+    return function (target: Function) {
+        Reflect.ownKeys(target.prototype).forEach(key => {
+            const descriptor = Reflect.getOwnPropertyDescriptor(target.prototype, key);
+            if (descriptor) {
+                const {value} = descriptor;
+                if (!descriptor.configurable) return;
+                if (typeof value !== 'function') return;
+                if (value === target) return;
+
+                const path = Reflect.getMetadata('path', target.prototype, key)
+                const handler: () => void = value
+                Router.get(`${prefix}${path}`, handler)
+            }
+        })
+    }
 }
+
