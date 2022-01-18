5fa23e9b145dc0cf6c905e422b200afa5cd58749
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-19 01:55:39 +0630

    Using descriptor to prevent unsafe fuction

--- 07_server/src/controllers/decorators/routes.ts
+++ 07_server/src/controllers/decorators/routes.ts
@@ -1,14 +1,19 @@
 import 'reflect-metadata'
 import {Methods} from "./Methods";
 import {MetadataKeys} from "./MetadataKeys";
+import {RequestHandler} from "express";
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+	value?: RequestHandler;
+}
 
 function routeBinder(method: string) {
-    return function (path: string) {
-        return function (target: Object, key: string) {
-            Reflect.defineMetadata(MetadataKeys.path, path, target, key);
-            Reflect.defineMetadata(MetadataKeys.method, method, target, key);
-        }
-    }
+	return function (path: string) {
+		return function (target: Object, key: string, descriptor: RouteHandlerDescriptor) {
+			Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+			Reflect.defineMetadata(MetadataKeys.method, method, target, key);
+		}
+	}
 }
 
 export const Get = routeBinder(Methods.get)
